b948d4c1e37a2b9265bebf52b245c9b7581988bd
BHARATHKUMARP76 <bharathkumar7604803804@gmail.com>
2026-04-17 22:24:34 +0530

    fix api base url for render

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://academic-learning-outcome-confidence-cqeb.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
